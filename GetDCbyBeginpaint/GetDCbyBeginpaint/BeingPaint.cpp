ac8d46f2d267a302527af27dc20bb2948bc4bb28
현청 박 <qkrguscjd2@naver.com>
2020-05-12 16:06:50 +0900

    실습 2-1

---
+++ GetDCbyBeginpaint/GetDCbyBeginpaint/BeingPaint.cpp
@@ -0,0 +1,61 @@
+//To get Divice context
+//Code by HyunChung Park
+
+#include <Windows.h>
+#include <TCHAR.H>
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow) {
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS wndclass;
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = _T("Window Class Name");
+	RegisterClass(&wndclass);
+	hwnd = CreateWindow(_T("Window Class Name"),
+		_T("HyunChung Window"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;//HDC Type manages memory section and can store attribute value about display section from memory section
+	PAINTSTRUCT ps;//structure to store information about output section
+
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		hdc = BeginPaint(hwnd, &ps);
+		//happen output in here
+		EndPaint(hwnd, &ps); // you must call this if you call BeginPaint function
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
+
